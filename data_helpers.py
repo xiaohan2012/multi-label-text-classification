25b864b05d90255ed3c8c0bd35a6856902efec72
Han Xiao <han.xiao@aalto.fi>
2017-08-14 20:30:55 +0000

    data preprocessing

---
+++ data_helpers.py
@@ -0,0 +1,43 @@
+import re
+from html.parser import HTMLParser
+
+
+def clean_str(string):
+    """
+    Tokenization/string cleaning for all datasets except for SST.
+    Original taken from https://github.com/yoonkim/CNN_sentence/blob/master/process_data.py
+    """
+    string = re.sub(r"[^A-Za-z0-9(),!?\'\`]", " ", string)
+    string = re.sub(r"\'s", " \'s", string)
+    string = re.sub(r"\'ve", " \'ve", string)
+    string = re.sub(r"n\'t", " n\'t", string)
+    string = re.sub(r"\'re", " \'re", string)
+    string = re.sub(r"\'d", " \'d", string)
+    string = re.sub(r"\'ll", " \'ll", string)
+    string = re.sub(r",", " , ", string)
+    string = re.sub(r"!", " ! ", string)
+    string = re.sub(r"\(", " \( ", string)
+    string = re.sub(r"\)", " \) ", string)
+    string = re.sub(r"\?", " \? ", string)
+    string = re.sub(r"\s{2,}", " ", string)
+    return string.strip().lower()
+
+
+class MLStripper(HTMLParser):
+    def __init__(self):
+        self.reset()
+        self.strict = False
+        self.convert_charrefs= True
+        self.fed = []
+
+    def handle_data(self, d):
+        self.fed.append(d)
+
+    def get_data(self):
+        return ''.join(self.fed)
+
+
+def strip_tags(html):
+    s = MLStripper()
+    s.feed(html)
+    return s.get_data()
---
+++ process_posts.py
@@ -0,0 +1,81 @@
+# coding: utf-8
+
+import os
+import re
+import pandas as pd
+import itertools
+import tensorflow as tf
+from data_helpers import strip_tags, clean_str
+
+
+tf.flags.DEFINE_string('data_dir', '', 'directory of dataset')
+tf.flags.DEFINE_integer('tag_freq_threshold', 5, 'minimum frequency of a tag')
+
+FLAGS = tf.flags.FLAGS
+FLAGS._parse_flags()
+
+data_dir = FLAGS.data_dir
+tag_freq_threshold = FLAGS.tag_freq_threshold
+
+
+label_path = os.path.join(data_dir, "labels.csv")
+text_path = os.path.join(data_dir, "input_text.csv")
+df = pd.read_csv('{}/posts.csv'.format(data_dir), sep=',')
+
+
+print("dataset containing {} records".format(df.shape[0]))
+
+
+qs = df[df['PostTypeId'] == 1]
+
+print("contains {} questions".format(qs.shape[0]))
+
+# extract tags
+regexp = re.compile("<(.+?)>")
+
+
+def extract_tags(s):
+    return regexp.findall(s)
+
+tags = qs["Tags"].apply(extract_tags).tolist()
+
+# filter out infrequent tags
+tag_freq = pd.Series(list(itertools.chain(*tags))).value_counts()
+valid_tags = tag_freq.index[tag_freq > tag_freq_threshold]
+tag_set = set(valid_tags)
+
+
+print('number of unique labels (frequency>{}): {}'.format(
+    tag_freq_threshold, len(tag_set)))
+
+
+normalized_tags = [[t for t in ts if t in tag_set] for ts in tags]
+
+
+# save labels to file
+y = pd.Series(list(map(lambda l: ",".join(l), normalized_tags)))
+
+mask = (y.apply(len) > 0)
+
+y = y[mask]
+qs.index = mask.index
+qs = qs[mask]
+
+assert y.shape[0] == qs.shape[0]
+
+print('num. questions with at least one valid labels: {}'.format(qs.shape[0]))
+
+print('saving labels to {}'.format(label_path))
+y.to_csv(label_path, index=False)
+
+
+body = qs['Body'].apply(strip_tags).apply(clean_str)
+title = qs['Title'].apply(strip_tags).apply(clean_str)
+
+# concatenate the texts
+input_text = pd.Series([' '.join(l) for l in list(zip(title, body))])
+
+
+print("saving input text to {}".format(text_path))
+input_text.to_csv(text_path)
+
