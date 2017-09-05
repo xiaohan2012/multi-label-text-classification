dc1729e7d7f196eacbbaa2b337561f6686cdb687
Han Xiao <han.xiao@aalto.fi>
2017-09-05 20:28:24 +0000

    experimenting with kim_cnn, however, tf_precision_at_k argument cannot be label list of different length at the last dim, such as [[1], [2, 3]]

---
+++ kim_cnn.py
@@ -0,0 +1,108 @@
+"""
+adapted from:
+
+- https://raw.githubusercontent.com/xiaohan2012/cnn-text-classification-tf/master/text_cnn.py
+- Jingzhou, Liu, etc, Deep Learning for Extreme Multi-label Text Classification, SIGIR 2017
+
+"""
+import tensorflow as tf
+from eval_helpers import tf_precision_at_k
+
+
+class KimCNN(object):
+    """
+    A CNN for text classification.
+    Uses an embedding layer, followed by a convolutional, max-pooling and softmax layer.
+    """
+    def __init__(
+            self, sequence_length, num_classes, vocab_size,
+            embedding_size, filter_sizes, num_filters, l2_reg_lambda=0.0,
+            loss_func='softmax'):
+
+        # Placeholders for input, output and dropout
+        self.input_x = tf.placeholder(tf.int32, [None, sequence_length], name="input_x")
+
+        # label indicator matrix
+        self.input_y_binary = tf.placeholder(tf.float32, [None, num_classes], name="input_y_binary")
+
+        # label list, shape is not defined
+        self.input_y_labels = tf.placeholder(tf.int32, shape=[None, None], name='input_y_labels')
+
+        self.dropout_keep_prob = tf.placeholder(tf.float32, name="dropout_keep_prob")
+
+        # Keeping track of l2 regularization loss (optional)
+        l2_loss = tf.constant(0.0)
+
+        # Embedding layer
+        with tf.device('/cpu:0'), tf.name_scope("embedding"):
+            self.W = tf.Variable(
+                tf.random_uniform([vocab_size, embedding_size], -1.0, 1.0),
+                name="W")
+            self.embedded_chars = tf.nn.embedding_lookup(self.W, self.input_x)
+            self.embedded_chars_expanded = tf.expand_dims(self.embedded_chars, -1)
+
+        # Create a convolution + maxpool layer for each filter size
+        pooled_outputs = []
+        for i, filter_size in enumerate(filter_sizes):
+            with tf.name_scope("conv-maxpool-%s" % filter_size):
+                # Convolution Layer
+                filter_shape = [filter_size, embedding_size, 1, num_filters]
+                W = tf.Variable(tf.truncated_normal(filter_shape, stddev=0.1), name="W")
+                b = tf.Variable(tf.constant(0.1, shape=[num_filters]), name="b")
+                conv = tf.nn.conv2d(
+                    self.embedded_chars_expanded,
+                    W,
+                    strides=[1, 1, 1, 1],
+                    padding="VALID",
+                    name="conv")
+                # Apply nonlinearity
+                h = tf.nn.relu(tf.nn.bias_add(conv, b), name="relu")
+                # Maxpooling over the outputs
+                pooled = tf.nn.max_pool(
+                    h,
+                    ksize=[1, sequence_length - filter_size + 1, 1, 1],
+                    strides=[1, 1, 1, 1],
+                    padding='VALID',
+                    name="pool")
+                pooled_outputs.append(pooled)
+
+        # Combine all the pooled features
+        num_filters_total = num_filters * len(filter_sizes)
+        self.h_pool = tf.concat(pooled_outputs, 3)
+        self.h_pool_flat = tf.reshape(self.h_pool, [-1, num_filters_total])
+
+        # Add dropout
+        with tf.name_scope("dropout"):
+            self.h_drop = tf.nn.dropout(self.h_pool_flat, self.dropout_keep_prob)
+
+        # Final (unnormalized) scores and predictions
+        with tf.name_scope("output"):
+            W = tf.get_variable(
+                "W",
+                shape=[num_filters_total, num_classes],
+                initializer=tf.contrib.layers.xavier_initializer())
+            b = tf.Variable(tf.constant(0.1, shape=[num_classes]), name="b")
+            l2_loss += tf.nn.l2_loss(W)
+            l2_loss += tf.nn.l2_loss(b)
+            self.scores = tf.nn.xw_plus_b(self.h_drop, W, b, name="scores")
+            self.predictions = tf.argmax(self.scores, 1, name="predictions")
+
+        # CalculateMean cross-entropy loss
+        with tf.name_scope("loss"):
+            if loss_func == 'sigmoid':
+                print('use sigmoid xentropy')
+                losses = tf.nn.sigmoid_cross_entropy_with_logits(logits=self.scores,
+                                                                 labels=self.input_y_binary)
+            elif loss_func == 'softmax':
+                print('use softmax xentropy')
+                losses = tf.nn.softmax_cross_entropy_with_logits(logits=self.scores,
+                                                                 labels=self.input_y_binary)
+            else:
+                raise ValueError('invalid loss function')
+            self.loss = tf.reduce_mean(losses) + l2_reg_lambda * l2_loss
+
+        # Accuracy
+        with tf.name_scope("performance"):
+            self.p1 = tf_precision_at_k(self.scores, self.input_y_labels, k=1, name='p1')
+            self.p3 = tf_precision_at_k(self.scores, self.input_y_labels, k=3, name='p3')
+            self.p5 = tf_precision_at_k(self.scores, self.input_y_labels, k=5, name='p5')
