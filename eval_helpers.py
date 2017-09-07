a136cafdce4bb243738cf54a8b58f70497ce5893
Han Xiao <han.xiao@aalto.fi>
2017-09-07 07:42:41 +0000

    fixed bug in precision_at_k (non-tf version)

--- eval_helpers.py
+++ eval_helpers.py
@@ -4,13 +4,18 @@ import itertools
 
 
 def precision(p, t):
-    """>>> precision({1, 2, 3, 4}, {1})
-       0.25
+    """
+    p, t: two sets of labels/integers
+    >>> precision({1, 2, 3, 4}, {1})
+    0.25
     """
     return len(t.intersection(p)) / len(p)
 
 
 def precision_at_ks(Y_pred_scores, Y_test, ks=[1, 3, 5, 10]):
+    """
+    Y_pred_scores: sparse matrix of dtype float, entry ij is the score of label j for instance i
+    """
     k = 3
     for k in [1, 3, 5, 10]:
         Y_pred = []
@@ -18,7 +23,7 @@ def precision_at_ks(Y_pred_scores, Y_test, ks=[1, 3, 5, 10]):
             idx = np.argsort(Y_pred_scores[i].data)[::-1]
             Y_pred.append(set(Y_pred_scores[i].indices[idx[:k]]))
 
-        precision_at_k = np.mean([precision(set(yt), yp) for yt, yp in zip(Y_test, Y_pred)])
+        precision_at_k = np.mean([precision(yp, set(yt)) for yt, yp in zip(Y_test, Y_pred)])
         print('precision at {}: {}'.format(k, precision_at_k))
 
 
