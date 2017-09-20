0ea8b46f84e739b7bce784be0d08e71f729fdc22
Han Xiao <han.xiao@aalto.fi>
2017-09-20 20:38:06 +0000

    model constructor, todo: train

--- kim_cnn.py
+++ kim_cnn.py
@@ -9,7 +9,7 @@ import tensorflow as tf
 from eval_helpers import tf_precision_at_k
 
 
-class KimCNN(object):
+class KimCNN():
     """
     A CNN for text classification.
     Uses an embedding layer, followed by a convolutional, max-pooling and softmax layer.
@@ -17,41 +17,63 @@ class KimCNN(object):
     def __init__(
             self, sequence_length, num_classes, vocab_size,
             embedding_size, filter_sizes, num_filters, l2_reg_lambda=0.0,
-            loss_function='softmax'):
+            loss_function='softmax',
+            redefine_output_layer=False):
 
+        self.sequence_length = sequence_length
+        self.num_classes = num_classes
+        self.vocab_size           = vocab_size
+        self.embedding_size       = embedding_size
+        self.filter_sizes         = filter_sizes
+        self.num_filters          = num_filters
+        self.l2_reg_lambda        = l2_reg_lambda
+        self.loss_function        = loss_function 
+        
         # Placeholders for input, output and dropout
         self.input_x = tf.placeholder(
-            tf.int32, [None, sequence_length], name="input_x")
+            tf.int32, [None, self.sequence_length], name="input_x")
 
         # label indicator matrix
         self.input_y_binary = tf.placeholder(
-            tf.float32, [None, num_classes], name="input_y_binary")
+            tf.float32, [None, self.num_classes], name="input_y_binary")
 
         # label list, a SparseTensor because label list length varies
         self.input_y_labels = tf.sparse_placeholder(
-            tf.int32, shape=[None, num_classes], name='input_y_labels')
+            tf.int32, shape=[None, self.num_classes], name='input_y_labels')
 
         self.dropout_keep_prob = tf.placeholder(tf.float32, name="dropout_keep_prob")
 
         # Keeping track of l2 regularization loss (optional)
-        l2_loss = tf.constant(0.0)
+        self.l2_loss = tf.constant(0.0)
 
+        # adding the layers
+        self.add_embedding_layer()
+        self.add_convolution_layer()
+        self.add_drop_out()
+
+        if not redefine_output_layer:
+            self.add_output()
+            self.add_loss()
+            self.add_performance()
+
+    def add_embedding_layer(self):
         # Embedding layer
         with tf.device('/cpu:0'), tf.name_scope("embedding"):
             self.W = tf.Variable(
-                tf.random_uniform([vocab_size, embedding_size], -1.0, 1.0),
+                tf.random_uniform([self.vocab_size, self.embedding_size], -1.0, 1.0),
                 name="W")
             self.embedded_chars = tf.nn.embedding_lookup(self.W, self.input_x)
             self.embedded_chars_expanded = tf.expand_dims(self.embedded_chars, -1)
 
+    def add_convolution_layer(self):
         # Create a convolution + maxpool layer for each filter size
         pooled_outputs = []
-        for i, filter_size in enumerate(filter_sizes):
+        for i, filter_size in enumerate(self.filter_sizes):
             with tf.name_scope("conv-maxpool-%s" % filter_size):
                 # Convolution Layer
-                filter_shape = [filter_size, embedding_size, 1, num_filters]
+                filter_shape = [filter_size, self.embedding_size, 1, self.num_filters]
                 W = tf.Variable(tf.truncated_normal(filter_shape, stddev=0.1), name="W")
-                b = tf.Variable(tf.constant(0.1, shape=[num_filters]), name="b")
+                b = tf.Variable(tf.constant(0.1, shape=[self.num_filters]), name="b")
                 conv = tf.nn.conv2d(
                     self.embedded_chars_expanded,
                     W,
@@ -64,47 +86,51 @@ class KimCNN(object):
                 # dim still 4d
                 pooled = tf.nn.max_pool(
                     h,
-                    ksize=[1, sequence_length - filter_size + 1, 1, 1],
+                    ksize=[1, self.sequence_length - filter_size + 1, 1, 1],
                     strides=[1, 1, 1, 1],
                     padding='VALID',
                     name="pool")
                 pooled_outputs.append(pooled)
 
         # Combine all the pooled features
-        num_filters_total = num_filters * len(filter_sizes)
+        self.num_filters_total = self.num_filters * len(self.filter_sizes)
         self.h_pool = tf.concat(pooled_outputs, 3)  # concat by last dim
-        self.h_pool_flat = tf.reshape(self.h_pool, [-1, num_filters_total])
+        self.h_pool_flat = tf.reshape(self.h_pool, [-1, self.num_filters_total])
 
+    def add_drop_out(self):
         # Add dropout
         with tf.name_scope("dropout"):
             self.h_drop = tf.nn.dropout(self.h_pool_flat, self.dropout_keep_prob)
 
+    def add_output(self):
         # Final (unnormalized) scores and predictions
         with tf.name_scope("output"):
             W = tf.get_variable(
                 "W",
-                shape=[num_filters_total, num_classes],
+                shape=[self.num_filters_total, self.num_classes],
                 initializer=tf.contrib.layers.xavier_initializer())
-            b = tf.Variable(tf.constant(0.1, shape=[num_classes]), name="b")
-            l2_loss += tf.nn.l2_loss(W)
-            l2_loss += tf.nn.l2_loss(b)
+            b = tf.Variable(tf.constant(0.1, shape=[self.num_classes]), name="b")
+            self.l2_loss += tf.nn.l2_loss(W)
+            self.l2_loss += tf.nn.l2_loss(b)
             self.scores = tf.nn.xw_plus_b(self.h_drop, W, b, name="scores")
             self.predictions = tf.argmax(self.scores, 1, name="predictions")
 
+    def add_loss(self):
         # CalculateMean cross-entropy loss
         with tf.name_scope("loss"):
-            if loss_function == 'sigmoid':
+            if self.loss_function == 'sigmoid':
                 print('use sigmoid xentropy')
                 losses = tf.nn.sigmoid_cross_entropy_with_logits(logits=self.scores,
                                                                  labels=self.input_y_binary)
-            elif loss_function == 'softmax':
+            elif self.loss_function == 'softmax':
                 print('use softmax xentropy')
                 losses = tf.nn.softmax_cross_entropy_with_logits(logits=self.scores,
                                                                  labels=self.input_y_binary)
             else:
                 raise ValueError('invalid loss function')
-            self.loss = tf.reduce_mean(losses) + l2_reg_lambda * l2_loss
+            self.loss = tf.reduce_mean(losses) + self.l2_reg_lambda * self.l2_loss
 
+    def add_performance(self):
         # Accuracy
         with tf.name_scope("performance"):
             self.p1 = tf_precision_at_k(self.scores, self.input_y_labels, k=1, name='p1')
--- word2vec.py
+++ word2vec.py
@@ -9,49 +9,39 @@ class Word2Vec():
 
     Args:
 
-    batch_size: int
     num_sampled: int, number of negative examples to sample
-    input_embedding: if given, `tf.Tensor`, used for the embedding table
-                     otherwise, construct it from `vocabulary_size` and `embedding_size`
     vocabulary_size: int
     embedding_size: int
     """
 
-    def __init__(self, batch_size,
+    def __init__(self,
                  num_sampled,
-                 input_embedding=None,
-                 vocabulary_size=None,
-                 embedding_size=None):
+                 vocabulary_size,
+                 embedding_size):
 
-        if input_embedding is not None:
-            vocabulary_size, embedding_size = map(lambda s: s.value,
-                                                  input_embedding.shape)
-        else:
-            assert vocabulary_size > 0
-            assert embedding_size > 0
+        self.vocabulary_size, self.embedding_size = (vocabulary_size,
+                                                     embedding_size)
+        assert self.vocabulary_size > 0
+        assert self.embedding_size > 0
 
         # Input data.
-        self.train_inputs = tf.placeholder(tf.int32, shape=[batch_size])
-        self.train_labels = tf.placeholder(tf.int32, shape=[batch_size, 1])
+        self.train_inputs = tf.placeholder(tf.int32, shape=None)
+        self.train_labels = tf.placeholder(tf.int32, shape=[None, 1])
 
         # Ops and variables pinned to the CPU because of missing GPU implementation
         with tf.device('/cpu:0'):
             # Look up self.embeddings for inputs.
             with tf.name_scope('embedding'):
-                if input_embedding is None:
-                    self.embeddings = tf.Variable(
-                        tf.random_uniform([vocabulary_size, embedding_size], -1.0, 1.0))
-                else:
-                    self.embeddings = tf.Variable(input_embedding)
-                    
+                self.embeddings = tf.Variable(
+                    tf.random_uniform([self.vocabulary_size, self.embedding_size], -1.0, 1.0))
                 embed = tf.nn.embedding_lookup(self.embeddings, self.train_inputs)
 
             with tf.name_scope('nce'):
                 # Construct the variables for the NCE loss
                 self.nce_weights = tf.Variable(
-                    tf.truncated_normal([vocabulary_size, embedding_size],
-                                        stddev=1.0 / math.sqrt(embedding_size)))
-                self.nce_biases = tf.Variable(tf.zeros([vocabulary_size]))
+                    tf.truncated_normal([self.vocabulary_size, self.embedding_size],
+                                        stddev=1.0 / math.sqrt(self.embedding_size)))
+                self.nce_biases = tf.Variable(tf.zeros([self.vocabulary_size]))
 
         # Compute the average NCE loss for the batch.
         # tf.nce_loss automatically draws a new sample of the negative labels each
@@ -63,7 +53,7 @@ class Word2Vec():
                                labels=self.train_labels,
                                inputs=embed,
                                num_sampled=num_sampled,
-                               num_classes=vocabulary_size))
+                               num_classes=self.vocabulary_size))
 
         norm = tf.sqrt(tf.reduce_sum(tf.square(self.embeddings), 1, keep_dims=True))
         self.normalized_embeddings = self.embeddings / norm
